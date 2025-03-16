b625bae0fbd878a79dc0b22a356d85b0c19b7401
ulvy11 <152696191+ulvy11@users.noreply.github.com>
2025-03-16 19:52:20 +0100

    feat(label_check) - ask if user want to modify unexpected label

--- label_check.py
+++ label_check.py
@@ -1,6 +1,16 @@
 from utils import *
 from label_rules import EXPECTED_LABELS, TO_CLASSIFY, TRANSFORMATION_RULES
 
+def change_label(mail_labeled: dict[str, str]) -> str:
+    if ask_user_yn(f"Change to {TO_CLASSIFY}?"):
+        mail_labeled[LABEL] = TO_CLASSIFY
+    else:
+        new_label_found = False
+        while not new_label_found:
+            new_label = input("Choose another label:")
+            if new_label_found := ask_user_yn(f"New label : '{new_label}'"):
+                mail_labeled[LABEL] = new_label
+
 def check_labels() -> None:
     mails_labeled = get_pickle(MAILS_LABELED_PICKLE_FILE)
     for mail_labeled in mails_labeled:
@@ -9,13 +19,7 @@ def check_labels() -> None:
             mail_labeled[LABEL] = TRANSFORMATION_RULES[label]
         elif label not in EXPECTED_LABELS:
             print(label)
-            if ask_user_yn(f"change to {TO_CLASSIFY}?"):
-                mail_labeled[LABEL] = TO_CLASSIFY
-            else:
-                new_label_found = False
-                while not new_label_found:
-                    new_label = input("Choose another label:")
-                    if new_label_found := ask_user_yn(f"new label : '{new_label}'"):
-                        mail_labeled[LABEL] = new_label
+            if ask_user_yn("Do you want to change the label?"):
+                change_label(mail_labeled)
 
     register_pickle(mails_labeled, MAILS_LABELED_PICKLE_FILE)
