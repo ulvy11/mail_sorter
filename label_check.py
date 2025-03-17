935e2eead76a6bebd836587e6a424ffbb3b06f82
ulvy11 <152696191+ulvy11@users.noreply.github.com>
2025-03-17 11:48:43 +0100

    feat(label check) - also check if in transformed labels

--- label_check.py
+++ label_check.py
@@ -1,6 +1,7 @@
 from utils import *
 from label_rules import EXPECTED_LABELS, TO_CLASSIFY, TRANSFORMATION_RULES
 
+
 def change_label(mail_labeled: dict[str, str]) -> str:
     if ask_user_yn(f"Change to {TO_CLASSIFY}?"):
         mail_labeled[LABEL] = TO_CLASSIFY
@@ -11,14 +12,15 @@ def change_label(mail_labeled: dict[str, str]) -> str:
             if new_label_found := ask_user_yn(f"New label : '{new_label}'"):
                 mail_labeled[LABEL] = new_label
 
+
 def check_labels() -> None:
     mails_labeled = get_pickle(MAILS_LABELED_PICKLE_FILE)
     for mail_labeled in mails_labeled:
         label: str = mail_labeled[LABEL]
         if label in TRANSFORMATION_RULES.keys():
-            print(f"Transform \"{label}\" -> \"{TRANSFORMATION_RULES[label]}\"")
+            print(f'Transform "{label}" -> "{TRANSFORMATION_RULES[label]}"')
             mail_labeled[LABEL] = TRANSFORMATION_RULES[label]
-        elif label not in EXPECTED_LABELS:
+        elif label not in [*EXPECTED_LABELS, *TRANSFORMATION_RULES.values()]:
             print(label)
             if ask_user_yn("Do you want to change the label?"):
                 change_label(mail_labeled)
